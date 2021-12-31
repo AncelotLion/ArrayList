4d10beb524508b1e7fa5868b0ae3cb850c3cfe48
AntonNikishin <ancelot.lion@gmail.com>
2021-12-31 16:04:39 +0300

    commit1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> food = new ArrayList<String>();
+
+        food.add("pizza");
+        food.add("hamburger");
+        food.add("hotdog");
+
+        //food.set(0, "sushi");
+        //Раскомментировать строку 17 для примера замещения значения
+
+        //food.remove(2);
+        //Раскомментировать строку 20 для примера удаления значения
+
+        //food.clear();
+        //Строка 19 - удаление значений
+
+        for(int i = 0; i < food.size(); i++) {
+            System.out.println(food.get(i));
+        }
+    }
+}
